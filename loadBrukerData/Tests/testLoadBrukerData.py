af9d9259f60d62d5139ec5ba7bde6a0a03629500
David Wise <d.wise.15@ucl.ac.uk>
2017-03-23 14:45:09 +0000

    Added unit tests

--- loadBrukerData/Tests/testLoadBrukerData.py
+++ loadBrukerData/Tests/testLoadBrukerData.py
@@ -1,6 +1,24 @@
 from nose.tools import assert_raises, assert_almost_equal, assert_equal
-from loadBrukerData.py import loadBrukerData
+import loadBrukerData.loadBrukerData as lbd
+import numpy as np
 
 def testDataType():
-	time, img, real = loadBrukerData('Fixtures/testData.dta')
-	assert_equal(type(time), 'numpy.ndarray')
+	time, img, real = lbd.loadBrukerData('Tests/Fixtures/testData.DTA')
+	print(str(type(time)))
+	assert_equal(str(type(time)), "<class 'numpy.ndarray'>")
+	assert_equal(str(type(img)), "<class 'numpy.ndarray'>")
+	assert_equal(str(type(real)), "<class 'numpy.ndarray'>")
+
+def testDataLength():
+	time, img, real = lbd.loadBrukerData('Tests/Fixtures/testData.DTA')
+	assert_equal(len(time), len(img), len(real))
+
+def testCorrectLoad():
+	correctData = np.load('Tests/Fixtures/npFixt.npz')
+	corTime = correctData['time']
+	corReal = correctData['real']
+	corImg = correctData['img']
+	time, img, real = lbd.loadBrukerData('Tests/Fixtures/testData.DTA')
+	assert_equal(time.all(), corTime.all())
+	assert_equal(img.all(), corImg.all())
+	assert_equal(real.all(), corReal.all())
